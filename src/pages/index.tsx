8e0d45044fbee852edf4755931678d32a1c66bd2
Ivan <ivan.ivicheff@yandex.com>
2023-04-01 18:35:39 +0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,10 @@ import { SignIn, SignInButton, SignOutButton, SignedIn, UserButton, SignedOut, u
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
   const user = useUser();
+
+  const {data} = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -21,7 +22,9 @@ const Home: NextPage = () => {
           {!user.isSignedIn && <SignInButton/>}
           {!!user.isSignedIn && <SignOutButton/>}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
